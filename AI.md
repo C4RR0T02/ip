8b604ce7507d0d7da199e662633cf750d0df8cc8
Sherlena Ngui <66990228+C4RR0T02@users.noreply.github.com>
2026-01-22 23:06:56 +0800

    Carrot Level 1: Echo

---
+++ AI.md
@@ -0,0 +1,4 @@
+# Carrot Levels AI Usage Documentation
+## Level-1: Echo
+- Aligning the text dynamically to the right
+
--- src/main/java/Carrot.java
+++ src/main/java/Carrot.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+
 public class Carrot {
+
+    public static boolean exit = false;
+    public static String seperator = "------------------------------------------";
+
+    public static void printLine() {
+        System.out.println(seperator);
+    }
 
     public static void printStart() {
         String logo = """
@@ -10,16 +19,31 @@ public class Carrot {
        """;
         System.out.println(logo);
         System.out.println("Hello! I'm C4RR0T\nWhat can I do for you?");
-        System.out.println("------------------------------------------");
+        printLine();
     }
 
     public static void printExit() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------------------");
+        printLine();
+    }
+
+    public static void echo() {
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        if (userInput.equalsIgnoreCase("bye")) {
+            exit = true;
+            printExit();
+        } else {
+            System.out.printf("%" + seperator.length() + "s%n", userInput);
+            printLine();
+        }
     }
 
     public static void main(String[] args) {
         printStart();
-        printExit();
+        while (!exit) {
+            echo();
+        }
+
     }
 }
