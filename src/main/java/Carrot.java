985d91e94062709f4491b0991cad7a37591c6021
Sherlena Ngui <66990228+C4RR0T02@users.noreply.github.com>
2026-01-22 23:53:18 +0800

    Carrot Level 2: Add, List

--- src/main/java/Carrot.java
+++ src/main/java/Carrot.java
@@ -1,9 +1,13 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Carrot {
 
+    public static final int LISTSIZE = 100;
+    public static int currentSize = 0;
     public static boolean exit = false;
     public static String seperator = "------------------------------------------";
+    public static String[] taskList = new String[LISTSIZE];
 
     public static void printLine() {
         System.out.println(seperator);
@@ -27,15 +31,30 @@ public class Carrot {
         printLine();
     }
 
+    public static void printList() {
+        for (int index = 1; index < (currentSize + 1); index++) {
+            System.out.println("\t" + index + " " + taskList[index]);
+        }
+    }
+
     public static void echo() {
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
-        if (userInput.equalsIgnoreCase("bye")) {
-            exit = true;
-            printExit();
-        } else {
-            System.out.printf("%" + seperator.length() + "s%n", userInput);
-            printLine();
+        switch (userInput.toLowerCase()) {
+            case "bye":
+                exit = true;
+                printExit();
+                break;
+            case "list":
+                printList();
+                break;
+            default:
+                taskList[currentSize + 1] = userInput;
+                userInput = "added: " + userInput;
+                System.out.printf("%" + seperator.length() + "s%n", userInput);
+                printLine();
+                currentSize++;
+                break;
         }
     }
 
